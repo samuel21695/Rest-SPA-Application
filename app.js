eed7372c6ed6521980b8e4e0a447cf568a55461e
samuel21695 <samuel21695@gmail.com>
2023-11-11 23:28:30 +0900

    ✨ const apiUlr && apiKey

---
+++ app.js
@@ -0,0 +1,2 @@
+const apiUrl = "http://apis.data.go.kr/6300000/openapi2022/tourspot";
+const apiKey = "jdJ4n%2BmtGnAwWjjroAWSRUmiCdIIIhT7KKsWAJ7Y0YCkPON4QyGZueSv41KqWkC9qn3p2pFqaOSYkezbhnWTBQ%3D%3D";
