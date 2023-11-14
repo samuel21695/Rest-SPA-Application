6723890642bfa7afddb4affde6651969638f0772
samuel21695 <samuel21695@gmail.com>
2023-11-14 16:34:30 +0900

    🔥 discard changes

--- openAPItest.js
+++ openAPItest.js
@@ -1,14 +1,14 @@
 // OpenAPI 엔드포인트 URL
-const apiURL = 'http://apis.data.go.kr/6300000/openapi2022/tourspot';
-const serviceKey = 'jdJ4n+mtGnAwWjjroAWSRUmiCdIIIhT7KKsWAJ7Y0YCkPON4QyGZueSv41KqWkC9qn3p2pFqaOSYkezbhnWTBQ=='
-const pageNo = 1
-const numOfRows = 10
+const apiUrl = 'https://apis.data.go.kr/6300000/openapi2022/tourspot/gettourspot?serviceKey=jdJ4n%2BmtGnAwWjjroAWSRUmiCdIIIhT7KKsWAJ7Y0YCkPON4QyGZueSv41KqWkC9qn3p2pFqaOSYkezbhnWTBQ%3D%3D&pageNo=1&numOfRows=10';
+// const serviceKey = 'jdJ4n+mtGnAwWjjroAWSRUmiCdIIIhT7KKsWAJ7Y0YCkPON4QyGZueSv41KqWkC9qn3p2pFqaOSYkezbhnWTBQ=='
+// const pageNo = 1
+// const numOfRows = 10
 
-// URL온성
-const apiUrlWithParams = `${apiURL}?serviceKey=${serviceKey}&pageNo=${pageNo}&numOfRows=${numOfRows}`;
+// // URL온성
+// const apiUrlWithParams = `${apiURL}?serviceKey=${serviceKey}&pageNo=${pageNo}&numOfRows=${numOfRows}`;
 
 // fetch 함수를 사용하여 데이터를 불러오기
-fetch(apiUrlWithParams)
+fetch(apiUrl)
   .then(response => {
     // HTTP 응답 코드 확인
     if (!response.ok) {
