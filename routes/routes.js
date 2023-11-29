a8f55332eb624fb1d1a642fac42248753d14df53
samuel21695 <samuel21695@gmail.com>
2023-11-29 21:00:44 +0900

    📝 routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.use('/static', express.static(path.join(__dirname, '..', 'static')));
 
 // route setting
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..','static', 'index.html'));
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'static', 'index.html'));
 })
 
 module.exports = router;
