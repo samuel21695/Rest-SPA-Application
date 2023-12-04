821f6bdc905ccfedd23adddcfb5e34202cd208d0
samuel21695 <samuel21695@gmail.com>
2023-12-04 17:23:23 +0900

    🎨 img src test

--- src/components/test.js
+++ src/components/test.js
@@ -2,13 +2,15 @@ import React from "react";
 
 function Body2() { 
   return (
-    <div className="w-70vw h-60vh bgc-g border-rad-20px display-f align-center justify-center">
+    <div className="w-70vw h-60vh display-f align-center justify-center">
       <div className="fs-4em">◀</div>
       <div>
-        <div className="w-50vw h-50vh bgc-w">
-          <div>
-            <div></div>
-            <ul>
+        <div className="w-50vw h-50vh bgc-w border-rad-20px">
+          <div className="w-50vw h-25vh display-f">
+            <div className="w-25vw h-25vh display-f align-center justify-center">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrApAJLpB0df7y6vzfsrZ8TSdHWJ9PrNzo2Q&usqp=CAU" className="w"></img>
+            </div>
+            <ul className="w-25vw h-25vh list-inside">
               <li></li>
               <li></li>
               <li></li>
@@ -16,8 +18,8 @@ function Body2() {
               <li></li>
             </ul>
           </div>
-          <div></div>
-          <div></div>
+          <div className="w-50vw h-10vh"></div>
+          <div className="w-50vw h-10vh"></div>
         </div>
       </div>
       <div className="fs-4em">▶</div>
