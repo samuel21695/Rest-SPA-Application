17e051a674aa5b12980ca2dc23540c4c059a1653
samuel21695 <samuel21695@gmail.com>
2023-12-04 14:58:03 +0900

    🚚 rename folder paths

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // 정적 파일 제공
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 모든 요청에 대해 index.html 제공
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
