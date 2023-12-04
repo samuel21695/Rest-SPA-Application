1bde0364c460e9ccb2c1d03cc0028ada4a60ae13
samuel21695 <samuel21695@gmail.com>
2023-12-04 19:53:51 +0900

    ✨ list style && align

--- src/components/test.js
+++ src/components/test.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-function TestImg() {
+function RandomDogImage() {
   const _URL = "https://dog.ceo/api/breeds/image/random";
   const [_randomDog, setRandomDog] = useState("");
 
@@ -13,7 +13,7 @@ function TestImg() {
   }, []);
     
     return(
-      <img src={_randomDog} className="w"></img>
+      <img src={_randomDog} className="w border-rad-20px"></img>
     )      
 }
 
@@ -22,16 +22,16 @@ function Body2() {
     <div className="w-70vw h-60vh display-f align-center justify-center">
       <div className="fs-4em">◀</div>
       <div>
-        <div className="w-50vw h-50vh bgc-w border-rad-20px">
-          <div className="w-50vw h-25vh display-f">
-            <div className="w-25vw h-25vh display-f align-center justify-center">
-              < TestImg/>
+        <div className="w-30vw h-50vh bgc-w border border-rad-20px ">
+          <div className="w-30vw h-25vh display-f">
+            <div className="w-30vw h-25vh display-f align-center justify-center">
+              < RandomDogImage/>
             </div>
-            <ul className="w-25vw h-25vh list-inside">
+            <ul className="w-25vw h-25vh list-inside display-f flex-column justify-center text">
               <li></li>
+              <br></br>
               <li></li>
-              <li></li>
-              <li></li>
+              <br></br>
               <li></li>
             </ul>
           </div>
