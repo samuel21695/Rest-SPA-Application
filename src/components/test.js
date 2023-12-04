8d0df5bded5bf04c7e9ff64fb63392eed0b6f66e
samuel21695 <samuel21695@gmail.com>
2023-12-04 16:42:37 +0900

    🎨 Body2 improvement

--- src/components/test.js
+++ src/components/test.js
@@ -5,7 +5,20 @@ function Body2() {
     <div className="w-70vw h-60vh bgc-g border-rad-20px display-f align-center justify-center">
       <div className="fs-4em">◀</div>
       <div>
-        <div id = "a" className="w-50vw h-50vh bgc-w"></div>
+        <div className="w-50vw h-50vh bgc-w">
+          <div>
+            <div></div>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
       <div className="fs-4em">▶</div>
     </div>
