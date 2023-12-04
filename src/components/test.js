737a9e91cdc72010c1a692ca7a6abda947455dac
samuel21695 <samuel21695@gmail.com>
2023-12-04 20:32:50 +0900

    🚧 layout in progress

--- src/components/test.js
+++ src/components/test.js
@@ -20,26 +20,29 @@ function RandomDogImage() {
 function Body2() { 
   return (
     <div className="w-70vw h-60vh display-f align-center justify-center">
-      <div className="fs-4em">◀</div>
+      <div className="fs-4em">‹</div>
       <div>
-        <div className="w-30vw h-50vh bgc-w border border-rad-20px ">
-          <div className="w-30vw h-25vh display-f">
+        <div className="w-30vw h-50vh bgc-w border border-rad-20px m-5vw display-f flex-column align-center">
+          <div className="w-30vw h-25vh display-f ">
             <div className="w-30vw h-25vh display-f align-center justify-center">
               < RandomDogImage/>
             </div>
-            <ul className="w-25vw h-25vh list-inside display-f flex-column justify-center text">
-              <li></li>
+            <ul className="w-25vw h-25vh list-inside display-f flex-column justify-center">
+              <li>Name: </li>
               <br></br>
-              <li></li>
+              <li>Age: </li>
               <br></br>
-              <li></li>
+              <li>Gender: </li>
             </ul>
           </div>
-          <div className="w-50vw h-10vh"></div>
-          <div className="w-50vw h-10vh"></div>
+          <div className="w-25vw h-10vh text-center"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta provident accusantium quos est natus fugiat dolor nobis minus iusto facilis, sed iure rerum doloribus corporis, tempora unde, eius velit aut.</div>
+          <div className="w-25vw h-10vh">
+            <a rel="http://example.com">link</a>
+            <button></button>
+          </div>
         </div>
       </div>
-      <div className="fs-4em">▶</div>
+      <div className="fs-4em">›</div>
     </div>
   );
 }
