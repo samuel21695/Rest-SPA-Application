1e8a36ce503ae647241747b9fc2adf3b40e289b1
samuel21695 <samuel21695@gmail.com>
2023-12-05 20:13:02 +0900

    ✨ create body1 (menu) div

---
+++ src/components/body1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Body1() { 
+  return (
+    <div class="w-15vw h-100vh bgc-lightgray">
+    </div>
+  );
+}
+
+export default Body1;
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Body2 from './components/test';
+import Body1 from './components/body1';
+import Body2 from './components/body2';
 
 function App() {
-  return <Body2 />
+  return (
+    <>
+    <Body1 />
+    <Body2 />
+    </>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
